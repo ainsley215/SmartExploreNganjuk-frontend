01a360f00bd101d3efab9dbef58e122405d2e5a6
ainsley215 <ainsley02121@gmail.com>
2026-06-14 22:23:16 +0700

    map

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -1,8 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// MEMPERBAIKI MASALAH IKON MARKER DEFAULT LEAFLET
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapSection() {
-  // Contoh data titik lokasi wisata
   const locations = [
     { id: 1, name: "Jolotundo Glamping", lat: -7.6, lng: 111.9 },
     { id: 2, name: "Taman Nyawiji", lat: -7.5, lng: 111.8 }
@@ -13,10 +25,17 @@ function MapSection() {
       <div className="max-w-7xl mx-auto px-6">
         <h2 className="text-4xl font-bold mb-8">Peta Lokasi</h2>
         
-        {/* Peta akan mengisi kontainer ini */}
-        <div className="rounded-3xl overflow-hidden h-[350px]">
-          <MapContainer center={[-7.6, 111.9]} zoom={12} style={{ height: "100%", width: "100%" }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* Kontainer Peta dengan tinggi yang didefinisikan */}
+        <div className="rounded-3xl overflow-hidden h-[350px] shadow-lg">
+          <MapContainer 
+            center={[-7.6, 111.9]} 
+            zoom={12} 
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer 
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
             
             {locations.map((loc) => (
               <Marker key={loc.id} position={[loc.lat, loc.lng]}>
@@ -29,3 +48,5 @@ function MapSection() {
     </section>
   )
 }
+
+export default MapSection;
