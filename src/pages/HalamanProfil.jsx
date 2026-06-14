482be82005fdb8947725c3761239850e0e5a1564
ainsley215 <ainsley02121@gmail.com>
2026-06-14 19:36:38 +0700

    membenahi typo

--- src/pages/HalamanProfil.jsx
+++ src/pages/HalamanProfil.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function Profil() {
